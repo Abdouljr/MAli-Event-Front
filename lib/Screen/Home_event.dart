ffe6985815639f54d17b0eaa0e95469feb13ce1f
Abdouljr <maigaabdoulaziz795@gmail.com>
2023-11-25 12:05:58 +0000

    init

---
+++ lib/Screen/Home_event.dart
@@ -0,0 +1,16 @@
+import 'package:flutter/src/widgets/container.dart';
+import 'package:flutter/src/widgets/framework.dart';
+
+class AccueilEvent extends StatefulWidget {
+  const AccueilEvent({super.key});
+
+  @override
+  State<AccueilEvent> createState() => _AccueilEventState();
+}
+
+class _AccueilEventState extends State<AccueilEvent> {
+  @override
+  Widget build(BuildContext context) {
+    return Container();
+  }
+}
