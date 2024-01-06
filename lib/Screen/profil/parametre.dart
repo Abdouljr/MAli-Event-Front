d232a85b332a1c233de6447388dc552e542c1acf
Camara <camaramamady9160@gmail.com>
2024-01-06 19:19:15 +0000

    parametre dans profil

---
+++ lib/Screen/profil/parametre.dart
@@ -0,0 +1,290 @@
+import 'package:flutter/material.dart';
+
+class Parametre extends StatefulWidget {
+  const Parametre({Key? key}) : super(key: key);
+
+  @override
+  State<Parametre> createState() => _ParametreState();
+}
+
+class _ParametreState extends State<Parametre> {
+  final _formKey = GlobalKey<FormState>();
+  final TextEditingController _nomController = TextEditingController();
+  final TextEditingController _prenomController = TextEditingController();
+    final TextEditingController _usernameController = TextEditingController();
+
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      backgroundColor: const Color(0xFF0F2D42),
+      body: Padding(
+        padding: const EdgeInsets.all(16.0),
+        child: SingleChildScrollView(
+          child: Form(
+            key: _formKey,
+            child: Column(
+              children: [
+                TextFormField(
+                  controller: _nomController,
+                  style: const TextStyle(color: Colors.white),
+                  decoration: const InputDecoration(
+                    labelText: 'Nom',
+                    labelStyle: TextStyle(color: Colors.white),
+                    enabledBorder: UnderlineInputBorder(
+                      borderSide:
+                          BorderSide(color: Color(0xFF0094ED), width: 15.0),
+                      borderRadius: BorderRadius.horizontal(
+                        left: Radius.circular(12.0),
+                        right: Radius.circular(12.0),
+                      ),
+                    ),
+                    focusedBorder: UnderlineInputBorder(
+                      borderSide:
+                          BorderSide(color: Color(0xFF0094ED), width: 15.0),
+                      borderRadius: BorderRadius.horizontal(
+                        left: Radius.circular(12.0),
+                        right: Radius.circular(12.0),
+                      ),
+                    ),
+                    errorStyle: TextStyle(
+                        color: Colors
+                            .white), // Couleur de la police en cas d'erreur
+                    errorBorder: UnderlineInputBorder(
+                      borderSide: BorderSide(
+                          color: Colors.red,
+                          width: 15.0), // Couleur de la bordure en cas d'erreur
+                      borderRadius: BorderRadius.horizontal(
+                        left: Radius.circular(12.0),
+                        right: Radius.circular(12.0),
+                      ),
+                    ),
+                    focusedErrorBorder: UnderlineInputBorder(
+                      borderSide: BorderSide(
+                          color: Colors.red,
+                          width:
+                              15.0), // Couleur de la bordure en cas d'erreur avec focus
+                      borderRadius: BorderRadius.horizontal(
+                        left: Radius.circular(12.0),
+                        right: Radius.circular(12.0),
+                      ),
+                    ),
+                  ),
+                  validator: (value) {
+                    if (value!.isEmpty) {
+                      return 'Veuillez entrer votre nom';
+                    }
+                    return null;
+                  },
+                ),
+                const SizedBox(height: 16.0),
+                  TextFormField(
+                  controller: _usernameController,
+                  style: const TextStyle(color: Colors.white),
+                  decoration: const InputDecoration(
+                    labelText: "Nom d'utilisateur",
+                    labelStyle: TextStyle(color: Colors.white),
+                    enabledBorder: UnderlineInputBorder(
+                      borderSide:
+                          BorderSide(color: Color(0xFF0094ED), width: 15.0),
+                      borderRadius: BorderRadius.horizontal(
+                        left: Radius.circular(12.0),
+                        right: Radius.circular(12.0),
+                      ),
+                    ),
+                    focusedBorder: UnderlineInputBorder(
+                      borderSide:
+                          BorderSide(color: Color(0xFF0094ED), width: 15.0),
+                      borderRadius: BorderRadius.horizontal(
+                        left: Radius.circular(12.0),
+                        right: Radius.circular(12.0),
+                      ),
+                    ),
+                    errorStyle: TextStyle(
+                        color: Colors
+                            .white), // Couleur de la police en cas d'erreur
+                    errorBorder: UnderlineInputBorder(
+                      borderSide: BorderSide(
+                          color: Colors.red,
+                          width: 15.0), // Couleur de la bordure en cas d'erreur
+                      borderRadius: BorderRadius.horizontal(
+                        left: Radius.circular(12.0),
+                        right: Radius.circular(12.0),
+                      ),
+                    ),
+                    focusedErrorBorder: UnderlineInputBorder(
+                      borderSide: BorderSide(
+                          color: Colors.red,
+                          width:
+                              15.0), // Couleur de la bordure en cas d'erreur avec focus
+                      borderRadius: BorderRadius.horizontal(
+                        left: Radius.circular(12.0),
+                        right: Radius.circular(12.0),
+                      ),
+                    ),
+                  ),
+                  validator: (value) {
+                    if (value!.isEmpty) {
+                      return "Veuillez entrer votre nom d'utilisateur";
+                    }
+                    return null;
+                  },
+                ),
+                const SizedBox(height: 16.0),
+                TextFormField(
+                  controller: _prenomController,
+                  style: const TextStyle(color: Colors.white),
+                  decoration: const InputDecoration(
+                    labelText: 'Prénom',
+                    labelStyle: TextStyle(color: Colors.white),
+                    enabledBorder: UnderlineInputBorder(
+                      borderSide:
+                          BorderSide(color: Color(0xFF0094ED), width: 15.0),
+                      borderRadius: BorderRadius.horizontal(
+                        left: Radius.circular(12.0),
+                        right: Radius.circular(12.0),
+                      ),
+                    ),
+                    focusedBorder: UnderlineInputBorder(
+                      borderSide:
+                          BorderSide(color: Color(0xFF0094ED), width: 15.0),
+                      borderRadius: BorderRadius.horizontal(
+                        left: Radius.circular(12.0),
+                        right: Radius.circular(12.0),
+                      ),
+                    ),
+                    errorStyle: TextStyle(
+                        color: Colors
+                            .white), // Couleur de la police en cas d'erreur
+                    errorBorder: UnderlineInputBorder(
+                      borderSide: BorderSide(
+                          color: Colors.red,
+                          width: 15.0), // Couleur de la bordure en cas d'erreur
+                      borderRadius: BorderRadius.horizontal(
+                        left: Radius.circular(12.0),
+                        right: Radius.circular(12.0),
+                      ),
+                    ),
+                    focusedErrorBorder: UnderlineInputBorder(
+                      borderSide: BorderSide(
+                          color: Colors.red,
+                          width:
+                              15.0), // Couleur de la bordure en cas d'erreur avec focus
+                      borderRadius: BorderRadius.horizontal(
+                        left: Radius.circular(12.0),
+                        right: Radius.circular(12.0),
+                      ),
+                    ),
+                  ),
+                  validator: (value) {
+                    if (value!.isEmpty) {
+                      return 'Veuillez entrer votre prénom';
+                    }
+                    return null;
+                  },
+                ),
+                const SizedBox(height: 16.0),
+                SizedBox(
+                   height: 50,
+                  width: double.infinity,
+                  child: ElevatedButton(
+                    onPressed: () {
+                      if (_formKey.currentState!.validate()) {
+                        // Le formulaire est valide, vous pouvez traiter les données ici
+                        String nom = _nomController.text;
+                                                String username = _usernameController.text;
+
+                        String prenom = _prenomController.text;
+                        // ignore: avoid_print
+                        print("Nom: $nom, Prénom: $prenom, Nom d'utilisateur:username");
+                      }
+                    },
+                    // ignore: sort_child_properties_last
+                    child: const Text('Modifier'),
+                    style: ElevatedButton.styleFrom(
+                      shape: RoundedRectangleBorder(
+                        borderRadius: BorderRadius.circular(15.0),
+                      ),
+                    ),
+                  ),
+                ),
+                const SizedBox(height: 50.0),
+                Container(
+                  // width: 50, // Largeur fixe de 50 pixels
+                  height: 50, // Hauteur fixe de 50 pixels
+                  decoration: BoxDecoration(
+                    color:const Color(0xFF0E3751), // Couleur de fond bleue
+                    borderRadius: BorderRadius.circular(
+                        12), // Bordure arrondie avec un rayon de 12 pixels
+                  ),
+                  child: Row(
+                    mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                    mainAxisSize: MainAxisSize.min,
+                    children: [
+                     const Padding(
+                        padding:  EdgeInsets.all(8.0),
+                        child: Icon(Icons.settings, color: Colors.white),
+                      ),
+                      Expanded(
+                        child: Container(
+                          color: Colors.transparent,
+                          child:const Center(
+                            child: Text(
+                              "Modifier le mot de passe",
+                              style: TextStyle(color: Colors.white),
+                            ),
+                          ),
+                        ),
+                      ),
+                     const Padding(
+                        padding:  EdgeInsets.all(8.0),
+                        child: Icon(Icons.arrow_forward_ios_sharp,
+                            color: Colors.white),
+                      ),
+                    ],
+                  ),
+                ),
+                 const SizedBox(height: 24.0),
+                Container(
+                  // width: 50, // Largeur fixe de 50 pixels
+                  height: 50, // Hauteur fixe de 50 pixels
+                  decoration: BoxDecoration(
+                    color: Color.fromARGB(255, 248, 1, 1), // Couleur de fond bleue
+                    borderRadius: BorderRadius.circular(
+                        12), // Bordure arrondie avec un rayon de 12 pixels
+                  ),
+                  child: Row(
+                    mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                    mainAxisSize: MainAxisSize.min,
+                    children: [
+                    const  Padding(
+                        padding:  EdgeInsets.all(8.0),
+                        child: Icon(Icons.login_outlined, color: Colors.white),
+                      ),
+                      Expanded(
+                        child: Container(
+                          color:  Colors.transparent,
+                          child:const Center(
+                            child: Text(
+                              "Déconnexion",
+                              style: TextStyle(color: Colors.white),
+                            ),
+                          ),
+                        ),
+                      ),
+                    const  Padding(
+                        padding:  EdgeInsets.all(8.0),
+                        child: Icon(Icons.arrow_forward_ios_sharp,
+                            color: Colors.white),
+                      ),
+                    ],
+                  ),
+                )
+              ],
+            ),
+          ),
+        ),
+      ),
+    );
+  }
+}
--- lib/Screen/profil/profil.dart
+++ lib/Screen/profil/profil.dart
@@ -1,141 +1,170 @@
 import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
-import 'package:flutter/src/widgets/framework.dart';
-import 'package:flutter/src/widgets/placeholder.dart';
+import 'package:front_mali_event/Screen/artistes/apropos.dart';
+import 'package:front_mali_event/Screen/artistes/publication.dart';
+import 'package:front_mali_event/Screen/profil/parametre.dart';
 
-class Profil extends StatelessWidget {
-  const Profil({super.key});
+class Profil extends StatefulWidget {
+  const Profil({Key? key}) : super(key: key);
+
+  @override
+  _ProfilState createState() => _ProfilState();
+}
+
+class _ProfilState extends State<Profil> with SingleTickerProviderStateMixin {
+  late TabController _tabController;
+
+  @override
+  void initState() {
+    super.initState();
+    _tabController = TabController(length: 2, vsync: this);
+  }
+
+  @override
+  void dispose() {
+    _tabController.dispose();
+    super.dispose();
+  }
 
   @override
   Widget build(BuildContext context) {
     return Scaffold(
       backgroundColor: Color(0xFF0F2D42),
-      body: Padding(
-        
-        padding: EdgeInsets.symmetric(
-          horizontal: MediaQuery.of(context).size.width *
-              0.015, // 5% de la largeur de l'écran
-        ),
+      body: SingleChildScrollView(
         child: Column(
           children: [
             SizedBox(height: 60),
-            Expanded(
-              child: Stack(
-                children: [
-                  Container(
-                    width: MediaQuery.of(context).size.width *
-                        1, // 90% de la largeur de l'écran (100% - 5% - 5%)
-                    height: 200,
-                    decoration: BoxDecoration(
-                      color: Color(0xFF0E3751),
-                      borderRadius: BorderRadius.circular(12),
-                    ),
-                    child: Padding(
-                      padding: const EdgeInsets.only(
-                        left: 50,
-                        top: 40,
-                        right: 16,
-                        bottom: 16,
-                      ),
-                      child: Column(
-                        crossAxisAlignment: CrossAxisAlignment.start,
-                        children: [
-                          SizedBox(height: 45),
-                          Text(
-                            'Sidiki Diabaté',
-                            style: TextStyle(
-                              fontSize: 25,
-                              fontWeight: FontWeight.bold,
-                              color: Colors.white,
-                            ),
-                          ),
-                          SizedBox(height: 20),
-                          Row(
-                            children: <Widget>[
-                              Text(
-                                'Dmusic ',
-                                style: TextStyle(
-                                    fontSize: 16, color: Colors.white),
-                              ),
-                              Expanded(
-                                child: Center(
-                                  child: Text(
-                                    'Tomikorobougou',
-                                    style: TextStyle(
-                                        fontSize: 16, color: Colors.white),
-                                  ),
-                                ),
-                              )
-                            ],
-                          ),
-                          SizedBox(height: 10),
-                          Row(
-                            children: <Widget>[
-                              Text(
-                                ' 91 22 88 29',
-                                style: TextStyle(
-                                    fontSize: 16, color: Colors.white),
-                              ),
-                            ],
-                          ),
-                        ],
-                      ),
-                    ),
+            Stack(
+              children: [
+                Container(
+                  width: MediaQuery.of(context).size.width * 1,
+                  height: 200,
+                  decoration: BoxDecoration(
+                    color: Color(0xFF0E3751),
+                    borderRadius: BorderRadius.circular(15),
                   ),
-                  Positioned(
-                    left: 16,
-                    
-                    child: Stack(
+                  child: Padding(
+                    padding: const EdgeInsets.only(
+                      left: 50,
+                      top: 40,
+                      right: 16,
+                    ),
+                    child: Column(
+                      crossAxisAlignment: CrossAxisAlignment.start,
                       children: [
-                        // Autres widgets ici
-                        Container(
-                          width: 80,
-                          height: 80,
-                          decoration: BoxDecoration(
-                            borderRadius: BorderRadius.circular(40 + 52),
-                            border: Border.all(
-                              width: 8,
-                              color: Color(0xFF1B252F),
-                            ),
-                          ),
-                          child: CircleAvatar(
-                            radius: 40,
-                            backgroundImage:
-                                AssetImage('assets/images/sdiki.jpeg'),
+                        SizedBox(height: 45),
+                        Text(
+                          'Sidiki Diabaté',
+                          style: TextStyle(
+                            fontSize: 25,
+                            fontWeight: FontWeight.bold,
+                            color: Colors.white,
                           ),
                         ),
-                        Positioned(
-                          top: 28,
-                          left: 45,
-                          child: Container(
-                            padding: EdgeInsets.all(4),
-                            decoration: BoxDecoration(
-                              shape: BoxShape.circle,
-                              color: Color(0xFF0094ED),
+                        SizedBox(height: 16),
+                        Row(
+                          children: <Widget>[
+                            Text(
+                              'Dmusic',
+                              style: TextStyle(
+                                  fontSize: 16, color: Colors.white),
                             ),
-                            child: Icon(
-                              Icons.edit_square,
-                              color: Colors.white,
-                              size: 30,
+                            Expanded(
+                              child: Center(
+                                child: Text(
+                                  'Tomikorobougou',
+                                  style: TextStyle(
+                                      fontSize: 16, color: Colors.white),
+                                ),
+                              ),
+                            )
+                          ],
+                        ),
+                        SizedBox(height: 10),
+                        Row(
+                          children: <Widget>[
+                            Text(
+                              ' 91 22 88 20',
+                              style: TextStyle(
+                                  fontSize: 16, color: Colors.white),
                             ),
-                          ),
+                          ],
                         ),
                       ],
                     ),
                   ),
-                  Positioned(
-                    right: 16,
-                    top: 16,
-                    child: Text(
-                      'Modifier',
-                      style: TextStyle(
-                        fontSize: 18,
-                        fontWeight: FontWeight.w300,
-                        color: Colors.blue,
+                ),
+                Positioned(
+                  left: 16,
+                  child: Stack(
+                    children: [
+                      Container(
+                        width: 80,
+                        height: 80,
+                        decoration: BoxDecoration(
+                          borderRadius: BorderRadius.circular(40 + 52),
+                          border: Border.all(
+                            width: 8,
+                            color: Color(0xFF1B252F),
+                          ),
+                        ),
+                        child: CircleAvatar(
+                          radius: 40,
+                          backgroundImage:
+                              AssetImage('assets/images/sdiki.jpeg'),
+                        ),
                       ),
+                      Positioned(
+                        top: 30,
+                        left: 49,
+                        child: Container(
+                          padding: EdgeInsets.all(4),
+                          decoration: BoxDecoration(
+                            shape: BoxShape.circle,
+                            color: Color(0xFF0094ED),
+                          ),
+                          child: Icon(
+                            Icons.edit_square,
+                            color: Colors.white,
+                            size: 22,
+                          ),
+                        ),
+                      ),
+                    ],
+                  ),
+                ),
+                Positioned(
+                  right: 16,
+                  top: 16,
+                  child: Text(
+                    'Modifier',
+                    style: TextStyle(
+                      fontSize: 18,
+                      fontWeight: FontWeight.w300,
+                      color: Colors.blue,
                     ),
                   ),
-                ],
+                ),
+              ],
+            ),
+            TabBar(
+              controller: _tabController,
+              tabs: [
+                Tab(text: 'Publications'),
+                Tab(text: 'Paramètre du compte'),
+              ],
+            ),
+            DefaultTabController(
+              length: 2,
+              child: SizedBox(
+                height: MediaQuery.of(context).size.height - 60 - 200 - kToolbarHeight,
+                child: TabBarView(
+                  controller: _tabController,
+                  children: [
+                    Publications(),
+                    Parametre(),
+                  ],
+                ),
               ),
             ),
           ],
