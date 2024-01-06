08550585177e67318f18f8be11f6946f065a0768
devBakary <bakdia194@gmail.com>
2024-01-06 18:37:18 +0000

    la page favorie artiste

--- lib/Screen/Favories/favorie artiste/favorie_artiste.dart
+++ lib/Screen/Favories/favorie artiste/favorie_artiste.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:front_mali_event/widget/fav%20titre.dart';
 
 import '../../../widget/entete_titre_icon.dart';
 
@@ -7,22 +8,29 @@ class ArtisteFav extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
+    Color bleu = Color(0xFF0164E5);
+    Color gris = Color(0xFFA0A9B0);
     return Scaffold(
         body: SingleChildScrollView(
       child: Padding(
-        padding: const EdgeInsets.all(10),
+        padding: EdgeInsets.all(10),
         child: Column(
+          crossAxisAlignment: CrossAxisAlignment.start,
           children: [
-            entete(),
+            favEntete(),
             SizedBox(height: 20),
             Container(
-              width: double.infinity,
-              height: 40,
-              decoration: BoxDecoration(
-                  border:
-                      Border.all(color: Colors.red, style: BorderStyle.solid),
-                  borderRadius: BorderRadius.circular(10)),
-            ),
+                width: double.infinity,
+                height: 40,
+                decoration: BoxDecoration(
+                    border: Border.all(color: gris, style: BorderStyle.solid),
+                    borderRadius: BorderRadius.circular(10)),
+                child: TextFormField(
+                  decoration: InputDecoration(
+                    prefixIcon: Icon(Icons.search_rounded),
+                    hintText: 'chercher artiste...',
+                  ),
+                )),
             SizedBox(height: 20),
             Row(
               children: [
@@ -55,11 +63,63 @@ class ArtisteFav extends StatelessWidget {
               height: 200,
               width: MediaQuery.of(context).size.width,
               decoration: BoxDecoration(
-                  color: Colors.deepPurple,
+                  image: DecorationImage(
+                      image: AssetImage("assets/images/sdiki.jpeg"),
+                      fit: BoxFit.cover),
                   borderRadius: BorderRadius.only(
                       topLeft: Radius.circular(10),
                       topRight: Radius.circular(10))),
             ),
+            SizedBox(
+              height: 10,
+            ),
+            Text("Concert",
+                style: TextStyle(
+                    color: bleu, fontSize: 24, fontWeight: FontWeight.w800)),
+            SizedBox(
+              height: 2,
+            ),
+            Row(
+              children: [
+                Text("Sidiki Diabaté",
+                    style: TextStyle(
+                        color: Colors.black,
+                        fontSize: 20,
+                        fontWeight: FontWeight.w700)),
+                SizedBox(
+                  width: 5,
+                ),
+                Icon(Icons.check_box_outlined, size: 40),
+              ],
+            ),
+            SizedBox(
+              height: 3,
+            ),
+            Row(
+              children: [
+                Container(
+                  child: Row(children: [
+                    Icon(Icons.place, color: gris, size: 18),
+                    Text(
+                      "Place de cinquantenaire",
+                      style: TextStyle(color: gris, fontSize: 12),
+                    )
+                  ]),
+                ),
+                SizedBox(
+                  width: 15,
+                ),
+                Container(
+                  child: Row(children: [
+                    Icon(Icons.calendar_month_sharp, color: gris, size: 18),
+                    Text(
+                      "31-12-2024",
+                      style: TextStyle(color: gris, fontSize: 12),
+                    )
+                  ]),
+                )
+              ],
+            )
           ],
         ),
       ),
